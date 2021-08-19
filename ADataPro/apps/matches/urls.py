65b8c2caa5a156a9b43f01a3ff4cfa8610b8cd50
Firotss <jannadom8@gmail.com>
2021-08-20 00:16:09 +0300

    +CBV

--- ADataPro/apps/matches/urls.py
+++ ADataPro/apps/matches/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('next_matches/', views.weekend_matches, name = 'next_matches'),
-    path('all_matches/<str:id>/', views.all_matches, name = 'all_matches'),
-    path('ranking/<str:id>/', views.ranking, name = 'ranking'),
-    path('<str:id>/', views.search, name = 'search'),
-    path('<str:team_id>/<str:year_id>/', views.search_by_year, name = 'search_team_by_year'),
+    path('',  Index.as_view(template_name="matches/list.html"), name = 'index'),
+    path('all_matches/<str:id>/',  RankingOrAllMatches.as_view(template_name="matches/list.html"), name = 'all_matches'),
+    path('ranking/<str:id>/',  RankingOrAllMatches.as_view(template_name="matches/ranking.html"), name = 'ranking'),
+    path('ranking/',  Ranking.as_view(template_name="matches/ranking.html"), name = 'ranking'),
+    path('<str:id>/', Search.as_view(template_name="matches/list.html"), name = 'search'),
+    path('<str:team_id>/<str:year_id>/',  SearchByYear.as_view(template_name="matches/list.html"), name = 'search_team_by_year'),
 ]
--- ADataPro/apps/matches/views.py
+++ ADataPro/apps/matches/views.py
@@ -2,65 +2,74 @@ from django.db.models.aggregates import Sum
 from django.shortcuts import render
 from .models import Matches, teams, TeamsInfo, update_db, next_matches
 from django.utils import timezone
+from django.views.generic import TemplateView
 
-def index(request):
-    matches_list = Matches.objects.all()
-    teams_list = teams()
-    update_db()
+class Index(TemplateView):
+    def get(self, request):
+        matches_list = Matches.objects.all()
+        teams_list = teams()
+        update_db()
 
-    return render(request, 'matches/list.html', 
-                    {'list': matches_list, 
-                    'teams_list': set(teams_list)})
+        return render(request, 'matches/list.html', 
+                        {'list': matches_list, 
+                        'teams_list': set(teams_list)})
 
+class Search(TemplateView):
+    def get(self, request, id):
+        matches_list = Matches.objects.all().filter(year = int(timezone.now().year))
+        teams_list = teams()
+        
+        if id == 'all_matches':
+            return render(request, self.template_name, {'list':matches_list, 'teams_list': set(teams_list)})
+        if id == 'next_matches':
+            weekend_list = next_matches()
+            return render(request, self.template_name, {'list':weekend_list, 'teams_list': set(teams_list)})
+        
+        searched_team = []
+        for actual_teams_list in matches_list.values():
+            if(actual_teams_list['team1_name'] == id or actual_teams_list['team2_name'] == id):
+                searched_team.append(actual_teams_list)
+        
+        return render(request, self.template_name, {'list':searched_team, 'teams_list': set(teams_list)})
+        
+class SearchByYear(TemplateView):
+    def get(self, request, team_id, year_id):
+        matches_list = Matches.objects.all().filter(year = int(year_id))
+        teams_list = teams()
 
-def search(request, id):
-    matches_list = Matches.objects.all().filter(year = int(timezone.now().year))
-    teams_list = teams()
+        searched_team = []
+        for actual_teams_list in matches_list.values():
+            if(actual_teams_list['team1_name'] == team_id or actual_teams_list['team2_name'] == team_id):
+                searched_team.append(actual_teams_list)
+        
+        return render(request, self.template_name, {'list':searched_team, 'teams_list': set(teams_list)})
     
-    if id == 'all_matches':
-        return render(request, 'matches/list.html', {'list':matches_list, 'teams_list': set(teams_list)})
-    if id == 'next_matches':
-        weekend_list = next_matches()
-        return render(request, 'matches/list.html', {'list':weekend_list, 'teams_list': set(teams_list)})
-    if id == 'ranking':
+class TeamsList(TemplateView):
+    def get(self, request, id):
+        teams_list = teams()
+        matches_list = Matches.objects.all().filter(year = int(id))
+        return render(request, self.template_name, {'list':matches_list, 'teams_list': set(teams_list)})
+
+class RankingOrAllMatches(TemplateView):
+    def get(self, request, id):
+        teams_list = teams()
+        if(self.template_name == "matches/list.html"):
+            matches_or_ranking_list = Matches.objects.all().filter(year = int(id))
+        else:
+            matches_or_ranking_list = TeamsInfo.objects.filter(date = int(id)).values('team_name').annotate(
+                                    wins=Sum('wins'), losses=Sum('losses'), 
+                                    draws=Sum('draws'), points=Sum('points')
+                                    ).order_by('wins', 'draws', 'points').reverse()
+        
+
+        return render(request, self.template_name, {'list':matches_or_ranking_list, 'teams_list': set(teams_list)})
+
+class Ranking(TemplateView):
+    def get(self, request):
+        teams_list = teams()
         ranking_list = TeamsInfo.objects.filter(date = int(timezone.now().year)).values('team_name').annotate(
-                            wins=Sum('wins'), losses=Sum('losses'), 
-                            draws=Sum('draws'), points=Sum('points')
-                            ).order_by('wins', 'draws', 'points').reverse()
-        return render(request, 'matches/ranking.html', {'list':ranking_list, 'teams_list': set(teams_list)})
-    
-    searched_team = []
-    for actual_teams_list in matches_list.values():
-        if(actual_teams_list['team1_name'] == id or actual_teams_list['team2_name'] == id):
-            searched_team.append(actual_teams_list)
-    
-    return render(request, 'matches/list.html', {'list':searched_team, 'teams_list': set(teams_list)})
-
-def search_by_year(request, team_id, year_id):
-    matches_list = Matches.objects.all().filter(year = int(year_id))
-    teams_list = teams()
-
-    searched_team = []
-    for actual_teams_list in matches_list.values():
-        if(actual_teams_list['team1_name'] == team_id or actual_teams_list['team2_name'] == team_id):
-            searched_team.append(actual_teams_list)
-    
-    return render(request, 'matches/list.html', {'list':searched_team, 'teams_list': set(teams_list)})
-    
-def all_matches(request, id):
-    teams_list = teams()
-    matches_list = Matches.objects.all().filter(year = int(id))
-    return render(request, 'matches/list.html', {'list':matches_list, 'teams_list': set(teams_list)})
-
-def weekend_matches(request):
-    teams_list = teams()
-    weekend_list = next_matches()
-    return render(request, 'matches/list.html', {'list':weekend_list, 'teams_list': set(teams_list)})
-
-def ranking(request, id):
-    teams_list = teams()
-    ranking_list = TeamsInfo.objects.filter(date = int(id)).values('team_name').annotate(
-                            wins=Sum('wins'), losses=Sum('losses'), 
-                            draws=Sum('draws'), points=Sum('points')
-                            ).order_by('wins', 'draws', 'points').reverse()
-    return render(request, 'matches/ranking.html', {'list':ranking_list, 'teams_list': set(teams_list)})
+                                wins=Sum('wins'), losses=Sum('losses'), 
+                                draws=Sum('draws'), points=Sum('points')
+                                ).order_by('wins', 'draws', 'points').reverse()
+        return render(request, self.template_name, {'list':ranking_list, 'teams_list': set(teams_list)})
+        
